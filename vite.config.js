486203684a5a8fd3a0b1a95a9ff3982d5a468b6b
Denys Shapoval <96021986+dawmsi@users.noreply.github.com>
2023-03-13 23:40:54 +0200

    change build dir

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import EnvironmentPlugin from 'vite-plugin-environment'
@@ -5,5 +6,8 @@ import dns from 'dns'
 
 dns.setDefaultResultOrder('verbatim')
 export default defineConfig({
+  build: {
+    outDir: path.join(__dirname, 'build')
+  },
   plugins: [react(), EnvironmentPlugin('all', { prefix: 'REACT_APP_' })]
 })
